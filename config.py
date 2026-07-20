caead9e17b30810e95869a66394497d5da054e1c
devdilip222-rgb <devdilip222@gmail.com>
2026-07-20 08:20:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,8 @@
 import os
+from os import environ
 
-API_ID = os.environ.get("API_ID", "")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
-API_HASH = os.environ.get("API_HASH", "")
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-PASS_DB = int(os.environ.get("PASS_DB", "721"))
-
-#OWNER = int(os.environ.get("OWNER", 7827463899))
-
-#LOG = -1002159628443,
-
-# UPDATE_GRP = , # bot sat group
-
-# auth_chats = []
-
-#try:
-    #ADMINS=[7827463899]
-    #for x in (os.environ.get("ADMINS", "7827463899").split()):
-        #ADMINS.append(int(x))
-#except ValueError:
-        #raise Exception("Your Admins list does not contain valid integers.")
-#ADMINS.append(OWNER)
+CREDIT = environ.get("CREDIT", "『ᴀᴅᴍɪɴ』")
